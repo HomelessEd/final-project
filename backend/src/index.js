08ff1203cd3b4335382b56eabe128a6dd10448ca
Edward Anderson <edward.a93@hotmail.co.uk>
2026-04-01 14:16:24 +0200

    First commit

---
+++ backend/src/index.js
@@ -0,0 +1,33 @@
+require  ('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: "Server is functioning"});
+});
+
+app.use('/api/posts', require('./routes/postRoutes'));
+
+app.use((req,res) => {
+    res.status(404).json({message: "Route not found"});
+});
+
+app.use(( err, req, res, next)=> {
+    console.error(err.stack);
+    const status = err.status || 500;
+    res.status(status).json({
+        error: {
+            message: err.message || "Server Error",
+            status: status
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server can be found at http://localhost:${PORT}`);
+});
